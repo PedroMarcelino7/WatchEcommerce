299d870a511f789279f4774e25076688f293ded4
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-21 17:19:00 -0300

    finishing best sellers

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -3,6 +3,7 @@ import './BestSellers.css'
 import Relogio1 from '../../images/relogio4.png'
 import Relogio2 from '../../images/relogio5.png'
 import Relogio3 from '../../images/relogio6.png'
+import Relogio4 from '../../images/relogio7.png'
 
 export default function BestSellers() {
     return (
@@ -23,7 +24,9 @@ export default function BestSellers() {
                             </div>
                         </div>
                     </div>
+                </div>
 
+                <div className="col-12 col-md-3">
                     <div className="card primary-bg-color">
                         <div className="card-img-top img-fluid">
                             <img className='card-img-top img-fluid' src={Relogio2} alt="Relógio 2" />
@@ -36,10 +39,27 @@ export default function BestSellers() {
                             </div>
                         </div>
                     </div>
+                </div>
 
+                <div className="col-12 col-md-3">
                     <div className="card primary-bg-color">
                         <div className="card-img-top img-fluid">
                             <img className='card-img-top img-fluid' src={Relogio3} alt="Relógio 3" />
+
+                            <div className="card-body">
+                                <p className="card-category secondary-color">Relógio</p>
+                                <h5 className="card-title">Nome do Produto</h5>
+                                <p className="card-text primary-color">R$177,77</p>
+                                <a href="" className="btn btn-primary">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-3">
+                    <div className="card primary-bg-color">
+                        <div className="card-img-top img-fluid">
+                            <img className='card-img-top img-fluid' src={Relogio4} alt="Relógio 4" />
 
                             <div className="card-body">
                                 <p className="card-category secondary-color">Relógio</p>
