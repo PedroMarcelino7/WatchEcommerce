011d8e96628f9a7826c98d2a9e7b023fe2cfddef
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-21 21:32:42 -0300

    finishing footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Banners from './components/Banners/Banners'
 import BestSellers from './components/BestSellers/BestSellers'
 import BottomNav from './components/BottomNav/BottomNav'
+import Footer from './components/Footer/Footer'
 import Gallery from './components/Gallery/Gallery'
 import Highlight from './components/Highlight/Highlight'
 import MiniBanners from './components/MiniBanners/MiniBanners'
@@ -26,6 +27,8 @@ function App() {
       <News />
 
       <Gallery />
+
+      <Footer />
     </>
   )
 }
